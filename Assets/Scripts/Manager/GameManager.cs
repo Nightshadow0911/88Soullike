6828eaa50cfbc8b63ca2098b6397b3f5d5c2e00a
SanyangForest <84529828+SanyangForest@users.noreply.github.com>
2023-10-30 15:58:52 +0900

    [edit]오류로 올라간거 재업로드 및 피격 추가
    [edit]오류로 올라간거 재업로드 및 피격 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+    // 캐릭터 스텟
+    public CharacterStats playerStats;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
