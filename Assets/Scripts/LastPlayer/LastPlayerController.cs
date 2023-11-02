4bf5b507b25bd116d268b4442f4b1ff9c8b53b4b
pjayas <115007528+pjayas@users.noreply.github.com>
2023-11-02 16:57:09 +0900

    Ladder

--- Assets/Scripts/LastPlayer/LastPlayerController.cs
+++ Assets/Scripts/LastPlayer/LastPlayerController.cs
@@ -9,13 +9,11 @@ public class LastPlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public FadeOut fadeOut;
 
-
     [SerializeField] private float speed = 5;
-    [SerializeField]private float jumpForce =10;
+    [SerializeField] private float jumpForce = 10;
 
     private bool canMove = true;
 
-   
     private bool canWallSlide;
     private bool isWallSliding;
 
@@ -24,14 +22,15 @@ public class LastPlayerController : MonoBehaviour
     private int facingDirection = 1;
     [SerializeField] private Vector2 wallJumpDirection;
 
-    //Ground check
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private LayerMask WhatIsLadder;
     [SerializeField] private float wallCheckDistance;
+    [SerializeField] private float ladderCheckdistance;
     private bool isGrounded;
     private bool isWallDetected;
+    private bool isLadderDetected;
 
-    //Dash
     [SerializeField] private float dashDistance = 10f;
     [SerializeField] private float dashDuration = 0.2f;
     [SerializeField] private float dashCooldown = 1f;
@@ -40,14 +39,10 @@ public class LastPlayerController : MonoBehaviour
     private float dashStartTime;
     private float lastDashTime;
 
-    //Stamina
-
     [SerializeField] private float maxStamina = 100f;
     [SerializeField] private float currentStamina;
     [SerializeField] private float staminaRegenRate = 10f;
     [SerializeField] private float dashStaminaCost = 20f;
-
-
 
     void Start()
     {
@@ -55,6 +50,7 @@ public class LastPlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         currentStamina = maxStamina;
     }
+
     void Update()
     {
         CheckInput();
@@ -65,7 +61,7 @@ public class LastPlayerController : MonoBehaviour
         if (isGrounded)
         {
             canMove = true;
-            RegenStamina(); // 땅에 있는 동안 스태미너 회복 추가
+            RegenStamina();
         }
 
         if (canWallSlide)
@@ -73,14 +69,23 @@ public class LastPlayerController : MonoBehaviour
             isWallSliding = true;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.1f);
         }
-        Move();
-        Dash();
-        Attack();
+
+        if (isLadderDetected)
+        {
+            ClimbLadder();
+        }
+        else
+        {
+            ReleaseLadder();
+            Move();
+            Dash();
+            Attack();
+        }
     }
 
     private void CheckInput()
     {
-            movingInput = Input.GetAxis("Horizontal");
+        movingInput = Input.GetAxis("Horizontal");
 
         if (Input.GetAxis("Vertical") < 0)
         {
@@ -91,16 +96,15 @@ public class LastPlayerController : MonoBehaviour
             JumpButton();
         }
     }
-    //Move
+
     private void Move()
     {
         if (canMove)
         {
-         rb.velocity = new Vector2(movingInput * speed, rb.velocity.y);
-         //fadeOut.makeFadeOut = true;
+            rb.velocity = new Vector2(movingInput * speed, rb.velocity.y);
         }
     }
-    //Dash
+
     private void Dash()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time > lastDashTime + dashCooldown)
@@ -112,7 +116,7 @@ public class LastPlayerController : MonoBehaviour
                 isDashing = true;
                 dashStartTime = Time.time;
                 lastDashTime = Time.time;
-                canMove = false; // 대시 중에 움직임 비활성화 (선택 사항)
+                canMove = false;
             }
         }
 
@@ -126,6 +130,7 @@ public class LastPlayerController : MonoBehaviour
             fadeOut.makeFadeOut = false;
         }
     }
+
     private void Attack()
     {
         if (Input.GetMouseButtonDown(0))
@@ -134,13 +139,11 @@ public class LastPlayerController : MonoBehaviour
         }
     }
 
-
     private void RegenStamina()
     {
         currentStamina += staminaRegenRate * Time.deltaTime;
         currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
     }
-
 
     private void JumpButton()
     {
@@ -162,8 +165,6 @@ public class LastPlayerController : MonoBehaviour
 
     private void wallJump()
     {
-        //WallJumpDirections flozen
-        //canMove = false;
         rb.velocity = new Vector2(wallJumpDirection.x * -facingDirection, wallJumpDirection.y);
     }
 
@@ -191,19 +192,20 @@ public class LastPlayerController : MonoBehaviour
     {
         bool isMoving = rb.velocity.x != 0;
 
-        anim.SetFloat("yVelocity",rb.velocity.y);
+        anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isWallSliding", isWallSliding);
         anim.SetBool("isWallDetected", isWallDetected);
-
     }
 
     private void CollisionCheck()
     {
-        isGrounded= Physics2D.Raycast(transform.position,Vector2.down, groundCheckDistance, whatIsGround);
-        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right*facingDirection, wallCheckDistance, whatIsGround);
-        if (isWallDetected && rb.velocity.y<0)
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDirection, wallCheckDistance, whatIsGround);
+        isLadderDetected = Physics2D.Raycast(transform.position, Vector2.up, ladderCheckdistance, WhatIsLadder);
+
+        if (isWallDetected && rb.velocity.y < 0)
         {
             canWallSlide = true;
         }
@@ -212,12 +214,39 @@ public class LastPlayerController : MonoBehaviour
             canWallSlide = false;
             isWallSliding = false;
         }
+
+        if (isLadderDetected)
+        {
+            rb.gravityScale = 0;
+        }
+    }
+
+    private void ClimbLadder()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (verticalInput > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+        else if (verticalInput < 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+
+    private void ReleaseLadder()
+    {
+        rb.gravityScale = 1;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + wallCheckDistance*facingDirection, transform.position.y));
-        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x,transform.position.y-groundCheckDistance));
-
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + wallCheckDistance * facingDirection, transform.position.y));
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
