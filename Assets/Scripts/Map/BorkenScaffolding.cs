8ca2ee7092ade7d3e074224d3371c90ddc425924
SanyangForest <84529828+SanyangForest@users.noreply.github.com>
2023-11-02 21:05:34 +0900

    [Add]부서지는 발판 추가
    [Add]부서지는 발판 추가

--- Assets/Scripts/Map/BorkenScaffolding.cs
+++ Assets/Scripts/Map/BorkenScaffolding.cs
@@ -1,41 +1,75 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BorkenScaffolding : MonoBehaviour
+public class BrokenScaffolding : MonoBehaviour
 {
     public AudioClip breakSound;
     private AudioSource audioSource;
+
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider2D;
+
+    private bool isDeactivated = false;
     private float delayInSeconds = 1.0f;
+    private Coroutine currentCoroutine;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = breakSound;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2D = GetComponent<Collider2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.CompareTag("Player"))
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0f);
+        foreach (Collider2D collider in colliders)
         {
-            StartCoroutine(DEActiveDelay(delayInSeconds));
-            PlaySound();
-            StartCoroutine(ActiveDelay(delayInSeconds));
+            if (collider.CompareTag("Player") && !isDeactivated)
+            {
+                if (currentCoroutine != null)
+                {
+                    StopCoroutine(currentCoroutine);
+                }
+                currentCoroutine = StartCoroutine(ProcessCollision());
+                break;
+            }
         }
     }
-    private IEnumerator DEActiveDelay(float delay)
+
+    private IEnumerator ProcessCollision()
     {
-        yield return new WaitForSeconds(delay);
-        gameObject.SetActive(false);
+        isDeactivated = true;
+        // 1. 1초 대기
+        yield return new WaitForSeconds(1.0f);
+
+        // 2. ToggleSpriteAndCollider(false);
+        ToggleSpriteAndCollider(false);
+
+        // 3. PlaySound();
+        PlaySound();
+
+        // 4. 1초 후 스프라이트와 콜라이더2D 다시 활성화
+        yield return new WaitForSeconds(3.0f);
+        ToggleSpriteAndCollider(true);
+        isDeactivated = false;
     }
-    private IEnumerator ActiveDelay(float delay)
+
+
+    private void PlaySound()
     {
-        yield return new WaitForSeconds(delay);
-        gameObject.SetActive(true);
-    }
-    void PlaySound()
-    {
+        // 사운드 재생
         audioSource.Play();
     }
+
+    private void ToggleSpriteAndCollider(bool isActive)
+    {
+        spriteRenderer.enabled = isActive;
+        collider2D.enabled = isActive;
+    }
+
+
 }
