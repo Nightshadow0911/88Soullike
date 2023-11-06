5e98dbdfea9e34e6a138ed21a7a2acad177864bc
SanyangForest <84529828+SanyangForest@users.noreply.github.com>
2023-11-06 11:27:38 +0900

    [Add]맵 기믹 추가 막힌 문 열림
    [Add]맵 기믹 추가 막힌 문 열림

--- Assets/Scripts/Map/DoorRemove.cs
+++ Assets/Scripts/Map/DoorRemove.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 
 public class DoorRemove : MonoBehaviour
 {
-    public GameObject removeDoor;
+    public GameObject player; 
+    public List<GameObject> removeDoors;
+    public float attackDistance = 2.0f;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // 좌클릭 확인
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        float distance = Vector3.Distance(player.transform.position, transform.position);
 
-            // Ray가 "Player" 태그를 가진 오브젝트와 "A" 오브젝트와 충돌한 경우
-            if (Physics.Raycast(ray, out hit) && (hit.collider.CompareTag("Player") || hit.collider.CompareTag("A")))
+        if (distance <= attackDistance) 
+        {
+            if (Input.GetMouseButtonDown(0)) 
             {
-                Debug.Log("11");
-                // "B" 오브젝트를 제거
-                if (removeDoor != null)
+                foreach (var door in removeDoors)
                 {
-                    Destroy(removeDoor);
+                    if (door != null)
+                    {
+                        door.SetActive(false);
+                    }
                 }
             }
         }
