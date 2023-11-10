55260e580b1affc75486b7c9d642218d673a2d32
LeeMinyeol <ingyeol16@gmail.com>
2023-11-10 20:06:24 +0900

    [CHORE]자잘

--- Assets/Scripts/Item/ItemDatabase.cs
+++ Assets/Scripts/Item/ItemDatabase.cs
@@ -22,4 +22,8 @@ public class ItemDatabase : MonoBehaviour
             go.GetComponent<FieldItems>().SetItem(itemDB[Random.Range(0, itemDB.Count)]);
         }
     }
+    // 박스와 몬스터에 아이템 정보를 세팅하는 함수
+    // 박스나 몬스터 파괴, 처치시에 세팅된 아이템을 떨구는 함수, 적당한 애니메이션 필요
+    // 박스.cs  -> 데미지가 얼마 들어오면 or 몇대 맞으면 사라짐, 사라지면서 디졸브 애니메이션, 열린상자로 이미지 변경
+    // 열린상자에 플레이어가 닿으면 아이템 획득(바로 획득할지?, 어떤걸 획득했다고 텍스트로 알려줄지?, 드랍템을 리스트로 띄워서 선택한 아이템을 획득하게 할지?)
 }
