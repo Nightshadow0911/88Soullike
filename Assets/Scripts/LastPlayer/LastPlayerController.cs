0a5399db336d2d2939e6e58934ce07b7b924fcf6
pjayas <115007528+pjayas@users.noreply.github.com>
2023-11-10 16:25:00 +0900

    fix

--- Assets/Scripts/LastPlayer/LastPlayerController.cs
+++ Assets/Scripts/LastPlayer/LastPlayerController.cs
@@ -121,7 +121,7 @@ public class LastPlayerController : MonoBehaviour
     {
         movingInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetAxis("Vertical") < 0)
+        if (Input.GetAxis("Vertical") < 0) //벽체크 
         {
             canWallSlide = false;
         }
@@ -307,14 +307,6 @@ public class LastPlayerController : MonoBehaviour
         rb.velocity = new Vector2(wallJumpDirection.x * -facingDirection, wallJumpDirection.y);
     }
 
-    private void Flip()
-    {
-        facingDirection = facingDirection * -1;
-        facingRight = !facingRight;
-
-        transform.Rotate(0, 180, 0);
-    }
-
     private void FlipController()
     {
         if (rb.velocity.x > 0 && !facingRight)
@@ -326,6 +318,14 @@ public class LastPlayerController : MonoBehaviour
             Flip();
         }
     }
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        facingRight = !facingRight;
+
+        transform.Rotate(0, 180, 0);
+    }
+
 
     private void ClimbLadder()
     {
