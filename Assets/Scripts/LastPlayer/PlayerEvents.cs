c90b53e58e0e9ed4c54d819ce1d035d458237b42
Nightshadow0911 <whqudgh0911@gmail.com>
2023-11-10 17:08:27 +0900

    [fix]콜라이더 istrigger 꺼져있던거 수정

--- Assets/Scripts/LastPlayer/PlayerEvents.cs
+++ Assets/Scripts/LastPlayer/PlayerEvents.cs
@@ -7,4 +7,5 @@ public class PlayerEvents : MonoBehaviour
 
     public static UnityAction<GameObject, int> playerDamaged;
     public static UnityAction<GameObject, int> playerHealed;
+    public static UnityAction<GameObject, int> monsterDamaged;
 }
