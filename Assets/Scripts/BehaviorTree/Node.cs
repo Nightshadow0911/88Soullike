04571ea0d6e9ccfe59018ef6f427b3b658baa539
KwakMinkyu <kmk3296@gmail.com>
2023-11-20 17:11:50 +0900

    수정중

---
+++ Assets/Scripts/BehaviorTree/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    public enum NodeState
+    {
+        RUNNING,
+        SUCCESS,
+        FAILURE
+    }
+    public class Node
+    {
+        
+    } 
+}
+
+
