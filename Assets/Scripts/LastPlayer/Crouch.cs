12840a70fd70db30470e5f904fec685687d0ac37
pjayas <115007528+pjayas@users.noreply.github.com>
2023-11-21 15:45:38 +0900

    Add[CrouchAttack]

--- Assets/Scripts/LastPlayer/Crouch.cs
+++ Assets/Scripts/LastPlayer/Crouch.cs
@@ -8,6 +8,8 @@ public class Crouch : MonoBehaviour
     private Vector2 normalHeight;
     public float crouchHeight;
     public LastPlayerController LastPlayerController;
+    public PlayerAttack playerAttack;
+
     // Start is called before the first frame update
     void Start()
     {
