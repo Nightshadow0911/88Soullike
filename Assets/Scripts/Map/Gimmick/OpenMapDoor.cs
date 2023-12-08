9da33a05b86767545637c84ce8ec2da3132a35a2
SanyangForest <84529828+SanyangForest@users.noreply.github.com>
2023-12-08 19:57:04 +0900

    [add]카메라 씬 체크
    [add]카메라 씬 체크

--- Assets/Scripts/Map/Gimmick/OpenMapDoor.cs
+++ Assets/Scripts/Map/Gimmick/OpenMapDoor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class OpenMapDoor : BaseGimmick
@@ -10,7 +11,12 @@ public class OpenMapDoor : BaseGimmick
     public List<Rigidbody2D> DoorRigidBody;
     public SpriteRenderer Img_Render;
     public Sprite Sprite01;
- 
+
+    public CinemachineVirtualCamera camera001;
+    public CinemachineVirtualCamera camera002;
+    [SerializeField]
+    public float Waittime;
+    
     
     protected override void Start()
     {
@@ -29,6 +35,7 @@ public class OpenMapDoor : BaseGimmick
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            CameraManager.instance.CutSenceCamera(camera001, camera002, Waittime);
             Img_Render.sprite = Sprite01;
             bool isCollision = mapGimmickInteraction.CollisionChecktoTagBased("Player", transform.position);
             if (isCollision)
