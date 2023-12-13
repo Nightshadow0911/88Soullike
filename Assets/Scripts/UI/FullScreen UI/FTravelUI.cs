170b6974c311a75b9ad208d4e1b42642c4e6f526
KwakMinkyu <kmk3296@gmail.com>
2023-12-13 15:25:11 +0900

    [add] 세이브 포인트 + 빠른이동
    ㅇㅇ

--- Assets/Scripts/UI/FullScreen UI/FTravelUI.cs
+++ Assets/Scripts/UI/FullScreen UI/FTravelUI.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Travel
+{
+    public string travelName;
+    public GameObject point;
+    [HideInInspector]
+    public Vector2 position;
+}
+
 public class FTravelUI : MonoBehaviour
 {
     public static FTravelUI instance;
+    private Dictionary<string, Travel> travelDict = new();
+    private Travel currentTravel;
+
+    [SerializeField] private Transform labelCreatePosition;
+    [SerializeField] private GameObject travelLabel;
+
+    public Action<bool> OnSavePoint;
+    public Action<Travel> OnTravel;
+    public Action OnReset;
 
     private void Awake()
     {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
         instance = this;
+
+        OnTravel += SetCurrentTravel;
+    }
+
+    public void SetTravel(Travel travel)
+    {
+        if (travelDict.ContainsKey(travel.travelName))
+            return;
+        travel.point.SetActive(true);
+        GameObject obj = Instantiate(travelLabel, labelCreatePosition);
+        travel.point.GetComponent<TravelPoint>().SetTravel(travel);
+        obj.GetComponent<TravelLabel>().SetTravel(travel);
+    }
+
+    public void SetCurrentTravel(Travel travel)
+    {
+        currentTravel = travel;
+    }
+
+    public void GoTravel()
+    {
+        GameManager.instance.player.transform.position = currentTravel.position;
+        OnSavePoint?.Invoke(false);
+        OnReset?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        OnReset?.Invoke();
     }
 }
--- Assets/Scripts/UI/MapUI.cs
+++ Assets/Scripts/UI/MapUI.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class MapUI : MonoBehaviour
 {
-    [SerializeField]private TravelPoint[] trs = new TravelPoint[4];
-    [SerializeField]private Transform trPointHolder;
-
-    private void OnEnable()
-    {
-        int trCount = trPointHolder.childCount;
-        for (int i = 0; i < trCount; i++)
-        {
-            trs[i] = trPointHolder.GetChild(i).GetComponent<TravelPoint>();
-            trs[i].CloseText();
-        }
-    }
+    
 }
